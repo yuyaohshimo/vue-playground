b8f0c81e5004c1f2be727e505d4e7ceba38c602c
yuyaohshimo <g2110008@gmail.com>
2017-05-11 03:17:34 +0900

    Add vuex-router-sync

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { sync } from 'vuex-router-sync';
 import App from './App';
 import store from './store';
 import router from './router';
+
+sync(store, router);
 
 Vue.config.productionTip = false;
 
